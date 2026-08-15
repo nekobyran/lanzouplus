1ae7dc814ffb1a53fb63746b0e60a9ed2ccf7f32
nekobyran <607234739+nekobyran@users.noreply.github.com>
2026-08-15 14:28:07 +0800

    Release LanzouPlus 1.2.9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,7 +7,7 @@ android {
  compileSdk = 36
  buildFeatures { buildConfig = true; aidl = true }
  androidResources { additionalParameters += listOf("--no-xml-namespaces", "--no-compile-sdk-metadata") }
- defaultConfig { applicationId = "cc.nkbr.lanzouplus"; minSdk = 24; targetSdk = 36; versionCode = 1002007; versionName = "1.2.7" }
+ defaultConfig { applicationId = "cc.nkbr.lanzouplus"; minSdk = 24; targetSdk = 36; versionCode = 1002009; versionName = "1.2.9" }
  flavorDimensions += "catalog"
  productFlavors {
   create("empty") {

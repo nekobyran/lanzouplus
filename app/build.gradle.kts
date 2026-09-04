4afbfd6fed7ddd668126d240799cf67f0cec0374
nekobyran <607234739+nekobyran@users.noreply.github.com>
2026-09-05 00:45:52 +0800

    Publish LanzouPlus 1.5.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,7 +7,7 @@ android {
  compileSdk = 36
  buildFeatures { buildConfig = true; aidl = true }
  androidResources { additionalParameters += listOf("--no-xml-namespaces", "--no-compile-sdk-metadata") }
- defaultConfig { applicationId = "cc.nkbr.lanzouplus"; minSdk = 24; targetSdk = 36; versionCode = 1005000; versionName = "1.5.0" }
+ defaultConfig { applicationId = "cc.nkbr.lanzouplus"; minSdk = 24; targetSdk = 36; versionCode = 1; versionName = "1.0.0" }
  flavorDimensions += "catalog"
  productFlavors {
   create("empty") {
